f615081a98712dbfab180fe26e98adae0aa4b55c
Hima Chhatrala <hima.chhatrala@gmail.com>
2022-10-15 15:29:04 +0100

    Initial commit with test

---
+++ Slice.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+//function to deliver pizza
+func pizzabot(_ : String) -> String {
+    return ""
+}
+
+
+//test
+assert(pizzabot("5x5 (1, 3) (4, 4)") == "ENNNDEEEND", "not equal")
