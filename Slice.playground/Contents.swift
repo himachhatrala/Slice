e40d42de1367d481ed4f7eb94f4420241cc9eb70
Hima Chhatrala <hima.chhatrala@gmail.com>
2022-10-16 17:46:36 +0100

    Extract grid size and drop locations from input string, validated them and added tests

--- Slice.playground/Contents.swift
+++ Slice.playground/Contents.swift
@@ -37,7 +37,7 @@ assert(test_input_string_matches_regex("25x35 (10, 3) (24, 34) (13, 9)") == true
 assert(test_input_string_matches_regex("100x100 (10, 3) (24, 34) (13, 9) (99, 99)") == true)
 assert(test_input_string_matches_regex("1000x1000 (10, 3) (24, 34) (13, 9) (99, 99) (100, 100) (999, 999)") == true)
 
-//Non Acceptable String tests
+//Tests for Non Acceptable String
 assert(test_input_string_matches_regex("7x7") == false) //expecting at least one drop locations
 assert(test_input_string_matches_regex("10xB (10, 3)") == false) //grid size should be in int
 assert(test_input_string_matches_regex("AxB (10, 3) (24, 34)") == false) //grid size should be in int
@@ -49,9 +49,60 @@ assert(test_input_string_matches_regex("100x100 (1,3) 24,34) 1, 100)") == false)
 assert(test_input_string_matches_regex("100x100 (1,3) 24,34 (1, 100") == false) //drop location should contain brackets
 
 
+//extract grid size
+func get_grid_size(_ input: String) -> [String : Int] {
+    let grid = input.split(separator: " ")[0]
+    
+    let grid_split = grid.split(separator: "x")
+    
+    let grid_size = ["x" : Int(grid_split[0])!, "y" : Int(grid_split[1])!]
+    
+    return grid_size
+}
+
+//Test for get grid size
+assert(get_grid_size("5x5 (1, 3) (4, 4)") == ["x": 5, "y": 5], "grid size is incorrect")
+assert(get_grid_size("100x100 (10, 3) (24, 34) (13, 9) (99, 99)") == ["x": 100, "y": 100], "grid size is incorrect")
+
+
+//extract drop locations from input string
+func get_drop_locations(_ input: String) -> [[String : Int]] {
+    let locations = input.split(separator: " ", maxSplits: 1)
+    
+    guard let points = locations.last?.components(separatedBy: CharacterSet.decimalDigits.inverted).compactMap ({ Int($0) })
+    else {
+        fatalError("Invalid points, please specify correct input points. Valid format: (x: 0, y: 0), (x: 1, y: 3)")
+    }
+    
+    var drop_locations: [[String : Int]] = []
+    
+    for index in stride(from: 0, to: points.count - 1, by: 2) {
+        drop_locations.append(["x": points[index], "y": points[index+1]])
+    }
+    
+    return drop_locations
+}
+
+//Test for get drop locations
+assert(get_drop_locations("5x5 (1, 3) (4, 4)") == [["x": 1, "y": 3], ["x": 4, "y": 4]], "drop locations are incorrect")
+assert(get_drop_locations("100x100 (10, 3) (24, 34) (13, 9) (99, 99)") == [["x": 10, "y": 3], ["x": 24, "y": 34], ["x": 13, "y": 9], ["x": 99, "y": 99]], "grid size is incorrect")
+
+
+//validate drop locations doesn't exceed grid size
+func validate_drop_locations(gridSize: [String : Int], dropLocations: [[String : Int]]) {
+    assert(dropLocations.allSatisfy { $0["x"]! <= gridSize["x"]! } , "exceeding x value")
+    assert(dropLocations.allSatisfy { $0["y"]! <= gridSize["y"]! } , "exceeding y value")
+}
 
 //function to deliver pizza
 func pizzabot(_ input: String) -> String {
+    
+    let grid_size = get_grid_size(input)
+    
+    let drop_locations = get_drop_locations(input)
+    
+    validate_drop_locations(gridSize: grid_size, dropLocations: drop_locations)
+    
     return ""
 }
 
